7698d49118865cb8360ab58a7464415bebefbb89
Jonnathan Montero <jesuslocopor@gmail.com>
2025-12-09 17:52:04 -0600

    Agrega setup.py final industrial

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cmfo_compute",
-    version="1.0.0",
+    name="cmfo-compute",
+    version="1.0.2",
+    author="Jonnathan Montero",
+    author_email="jesuslocopor@gmail.com",
+    description="CMFO Compute: Librería base de cálculo fractal T7 del sistema CMFO.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/1JONMONTERV/cmfo-compute",
     packages=find_packages(),
+    python_requires=">=3.9",
     install_requires=[
-        "numpy",
+        "numpy>=1.20",
     ],
-    author="CMFO Research",
-    description="Fractal Computing Library based on CMFO T7-phi automaton.",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
