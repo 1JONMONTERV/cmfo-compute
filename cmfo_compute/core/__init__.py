a9cdae6f5bcd1ed8e328cc19809484987ac447fe
Jonnathan Montero <jesuslocopor@gmail.com>
2025-12-09 18:20:27 -0600

    Fix: remove invalid phi_weights import from core initializer

--- cmfo_compute/core/__init__.py
+++ cmfo_compute/core/__init__.py
@@ -1,21 +1,7 @@
 """
-Core CMFO - Operador φ, matemáticas φ y T7Tensor
+Core CMFO Compute package exports.
 """
 
-from .phi_math import (
-    PHI,
-    phi_pow,
-    phi_weights,
-    phi_norm,
-)
+from .phi_math import PHI, phi_pow, phi_norm
 from .gamma_phi import gamma_step
 from .t7_tensor import T7Tensor
-
-__all__ = [
-    "PHI",
-    "phi_pow",
-    "phi_weights",
-    "phi_norm",
-    "gamma_step",
-    "T7Tensor",
-]
